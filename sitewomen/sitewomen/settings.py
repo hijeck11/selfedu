9dbd74c35f2a250a6f37d5fff81c9e09767aadf5
hijeck11 <pashkevich.anton.v@gmail.com>
2024-01-28 00:37:24 +0300

    в настройках изменил дебаг и указал локалхост создал файл конвертерс.пи

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-netl%h58+w1q6gp29o=sbp-=#u@k9&s5n^fpwa3$tlckx$jcm$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.urls import reverse
 
 # dfdfdfdf
 def index(request):
     return HttpResponse("Страница приложения women")
 
 
-def categories(request):
-    return HttpResponse("<h1>Cтатьи по категориям</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Cтатьи по категориям</h1><p>id: {cat_id}</p>")
+
+def categories_by_slug(request, cat_slug):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f"<h1>Cтатьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+def archive(request, year):
+    if year > 2023:
+        uri = reverse('cats', args=('music', ))
+        return HttpResponseRedirect(uri)
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
