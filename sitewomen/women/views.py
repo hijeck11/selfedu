59b6aa2a3783373518fab5f3f235a46ea8875570
hijeck11 <pashkevich.anton.v@gmail.com>
2024-05-12 01:23:59 +0300

    #8 redirect and HttpResponse

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
@@ -40,9 +40,10 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2023:
-        return redirect('/')
-
+        uri = reverse('cats', args=('sport', ))
+        return HttpResponsePermanentRedirect(uri)
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+# с permanent=True перенаправление ошибка 301, без 302
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
