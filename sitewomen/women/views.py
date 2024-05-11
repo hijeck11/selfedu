c8d2c5c525260574fe9cac5980005487fb105b63
hijeck11 <pashkevich.anton.v@gmail.com>
2024-05-12 00:23:25 +0300

    #7 Get and Post-запросы в настройках изменил дебаг и указал локалхост создал файл конвертерс.пи

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
-# dfdfdfdf
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Ввойти']
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
 def index(request):
-    return HttpResponse("Страница приложения women")
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+    data = {
+        'title': 'главная страница',
+        'main_title': 'title',
+        'menu': menu,
+        'float': 28.56,
+        'lst': [1, 2, 'abc', True],
+        'set': {1, 2, 3, 2, 5},
+        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+        'obj': MyClass(10, 20),
+    }
+    return render(request, 'women/index.html', context=data)
 
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 def categories(request, cat_id):
     return HttpResponse(f"<h1>Cтатьи по категориям</h1><p>id: {cat_id}</p>")
