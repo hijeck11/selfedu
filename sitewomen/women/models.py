ae1169b8db0acc609d12781ca43c56505b652498
hijeck11 <pashkevich.anton.v@gmail.com>
2024-06-16 16:10:04 +0300

    #23 Пользовательский менеджер.

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,16 +1,30 @@
 from django.urls import reverse
-
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
+
+
 class Women(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+    #Пользовательский менеджер
+
+
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
 
+    objects = models.Manager()
+    published = PublishManager()
     def __str__(self):
         return self.title
 
