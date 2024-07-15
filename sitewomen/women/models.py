4616577aea1511dbf7e8f8b9af231bb5da6d3b61
hijeck11 <pashkevich.anton.v@gmail.com>
2024-07-15 16:36:28 +0300

    #27 Отображеие постов по рубрикам

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -20,7 +20,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
-    cat = models.ForeignKey('Category', on_delete=models.CASCADE)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
 
     objects = models.Manager()
     published = PublishManager()
@@ -43,3 +43,6 @@ class Category(models.Model):
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
