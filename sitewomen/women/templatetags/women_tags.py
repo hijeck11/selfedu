3689bc66453d802914a2cf36a10d944b55f70895
hijeck11 <pashkevich.anton.v@gmail.com>
2024-07-27 15:12:14 +0300

    #35 Группировка записей. Вычилсения на стороне СУБД

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 import women.views as views
 from women.models import Category, TagPost
 
@@ -6,10 +7,10 @@ register = template.Library()
 
 @register.inclusion_tag('women/list_categories.html')
 def show_categories(cats_selected=0):
-    cats = Category.objects.all()
+    cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {'cats': cats, 'cat_selected': cats_selected}
 
 
 @register.inclusion_tag('women/list_tags.html')
 def show_all_tags():
-    return {'tags': TagPost.objects.all()}
+    return {'tags': TagPost.objects.annotate(total=Count("tags")).filter(total__gt=0)}
