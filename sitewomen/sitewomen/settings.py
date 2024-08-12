9b700deee0c7a358c263025a27731d594fd7c8f7
a.pashkevich <pashkevich.anton.v@gmail.com>
2024-08-12 09:17:21 +0300

    #49 Загрузка файлов на сервер в связке с моделью и через форму

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -127,6 +127,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+MEDIA_ROOT = BASE_DIR / 'media'
+# указываем каталог куда будут загружаться все файлы
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 
 from .forms import AddPostForm, UploadFileForm
-from .models import Women, Category, TagPost
+from .models import Women, Category, TagPost, UploadFiles
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'addpage'},
@@ -24,16 +24,17 @@ def index(request):
     }
     return render(request, 'women/index.html', context=data)
 
-def handle_uploaded_file(f):
-    with open(f"uploads/{f.name}", "wb+") as destination:
-        for chuk in f.chunks():
-            destination.write(chuk)
+# def handle_uploaded_file(f):
+#     with open(f"uploads/{f.name}", "wb+") as destination:
+#         for chuk in f.chunks():
+#             destination.write(chuk)
 
 def about(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(form.cleaned_data['file'])
+            fp = UploadFiles(file=form.cleaned_data['file'])
+            fp.save()
     else:
         form = UploadFileForm()
     return render(request, 'women/about.html',
@@ -53,7 +54,7 @@ def show_post(request, post_slug):
 
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             # print(form.cleaned_data)
             # try:
