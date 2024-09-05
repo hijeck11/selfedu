ebca01583850f818f17cbc920c7416ad95eff0d0
a.pashkevich <pashkevich.anton.v@gmail.com>
2024-09-05 17:08:42 +0300

    #67 классы PasswordChangeView и PasswordChangeDoneView

--- sitewomen/users/authentication.py
+++ sitewomen/users/authentication.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import BaseBackend
 
+
+
 class EmailAuthBackends(BaseBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         user_model = get_user_model()
@@ -19,3 +21,4 @@ class EmailAuthBackends(BaseBackend):
             return user_model.objects.get(pk=user_id)
         except user_model.DoesNotExist:
             return None
+
